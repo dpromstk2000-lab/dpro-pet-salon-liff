f4b7addf0b91130692893046d16adf8c692f483d
dpromstk2000-lab <dpromstk2000@gmail.com>
2026-07-22 16:29:55 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
-// STEP PETSALON-BRUSHUP-6
-// DPRO PET SALON LINE / QR受付・iPad受付
+// STEP PETSALON-BRUSHUP-7
 window.DPRO_PETSALON_CONFIG = Object.freeze({
   SHOP_CODE: "pet_salon_demo",
   LEGACY_WORKER_BASE_URL: "https://dpro-pet-salon-api.dpromstk2000.workers.dev",
@@ -7,5 +6,7 @@ window.DPRO_PETSALON_CONFIG = Object.freeze({
   DEMO_ADMIN_CODE: "1234",
   PUBLIC_CHECKIN_PAGE: "checkin.html",
   RECEPTION_PAGE: "reception.html",
-  CHECKIN_QR_IMAGE: "checkin-qr.png"
+  CHECKIN_QR_IMAGE: "checkin-qr.png",
+  REBOOK_PAGE: "repeat-booking.html",
+  REBOOK_ADMIN_PAGE: "rebook-admin.html"
 });
